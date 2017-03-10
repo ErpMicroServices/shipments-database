d52fb0a632fea33393a898965a1e68e9f9e185e5
Jim Barrows <Jim.Barrows@gmail.com>
2017-03-10 10:55:17 -0700

    shipment items

--- 02-create-database.sql
+++ 02-create-database.sql
@@ -22,3 +22,28 @@ create table if not exists shipment(
   inquired_about_via_contact_mechanism_id uuid not null,
   CONSTRAINT shipment_pk PRIMARY key(id)
 );
+
+create table if not exists shipment_item(
+  id uuid DEFAULT uuid_generate_v4(),
+  sequence bigint not null,
+  quantity bigint default 1,
+  content_description text,
+  generating uuid references shipment_item(id),
+  response_to uuid references shipment_item(id),
+  shipment_of_good_id uuid not null,
+  CONSTRAINT shipment_item_pk PRIMARY key(id)
+);
+
+create table if not exists shipment_status_type(
+  id uuid DEFAULT uuid_generate_v4(),
+  description text not null CONSTRAINT shipment_status_type_description_not_empty CHECK (description <> ''),
+  CONSTRAINT shipment_status_type_pk PRIMARY key(id)
+);
+
+create table if not exists shipment_status(
+  id uuid DEFAULT uuid_generate_v4(),
+  status_date date not null default current_date,
+  shipment_id uuid not null references shipment(id),
+  described_by uuid not null references shipment_status_type(id),
+  CONSTRAINT shipment_status_pk PRIMARY key(id)
+);
